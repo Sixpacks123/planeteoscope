891769a1f799d5806372b41e0c4259a6742dc1a2
Heurtault Aubin <aub.heurtault@gmail.com>
2023-04-07 09:15:02 +0200

    feat: add pages details  for one astres

---
+++ src/views/AstreDetail.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mt-20">
+    <h1>{{ astre.name }}</h1>
+    <p><strong>English name:</strong> {{ astre.englishName }}</p>
+    <p><strong>Is planet:</strong> {{ astre.isPlanet }}</p>
+    <!-- Afficher les autres informations de l'astre ici -->
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { useAstresStore } from '../store/astre'
+
+const astresStore = useAstresStore()
+const route = useRoute()
+const astreId = route.params.id
+
+const astre = computed(() => {
+  const astre = astresStore.getAstreById(astreId)
+  console.log(astreId)
+  return astre
+})
+</script>
+
+
