f39bede5dae9c2d01049ee1cbeb3b4a6c8c6187d
Heurtault Aubin <aub.heurtault@gmail.com>
2023-04-07 09:12:28 +0200

    chore: update param route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import Home from './views/Home.vue'
 import About from './views/About.vue'
 import NotFound from './views/NotFound.vue'
+import Astres from './views/Astres.vue'
+import AstreDetail from './views/AstreDetail.vue'
 
 export const routes = [
   { path: '/', component: Home, meta: { title: 'Home' }},
+  { path: '/astres', component: Astres, meta: { title: 'Astres' }},
+  { path: '/astres/:id', name: 'AstreDetail', component: AstreDetail, props: true },
   { path: '/about', component: About, meta: { title: 'About' }},
   { path: '/:pathMatch(.*)*', component: NotFound, meta: { title: 'Page not found' }},
 ]
