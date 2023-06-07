b19bba579378940784ce5aa41e78495505daadfc
Heurtault Aubin <aub.heurtault@gmail.com>
2023-06-07 16:02:27 +0200

    eslint fix

--- src/components/terre.vue
+++ src/components/terre.vue
@@ -11,7 +11,6 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-
 export default {
   data() {
     return {
