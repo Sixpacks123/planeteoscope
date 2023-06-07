8d140c2859b0b449f91cab08da931e860693c686
Heurtault Aubin <aub.heurtault@gmail.com>
2023-06-07 10:18:10 +0200

    import feature tostify motion

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import App from './App.vue'
 import { routes } from './routes'
 import './index.css'
 import { createPinia } from 'pinia'
+import { useAstresStore } from './store/astre'
+import Vue3Toasity, { type ToastContainerOptions } from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
+import { MotionPlugin } from '@vueuse/motion'
+
+
 const pinia = createPinia()
 
 const app = createApp(App)
@@ -30,6 +37,17 @@ if (import.meta.hot) {
     router.replace('')
   })
 }
+
 app.use(pinia)
 app.use(router)
+app.use(autoAnimatePlugin)
+app.use(
+    Vue3Toasity,
+    {
+      autoClose: 3000,
+      // ...
+    } as ToastContainerOptions,
+  )
+app.use(MotionPlugin)
 app.mount('#app')
+const astreStore = useAstresStore()
