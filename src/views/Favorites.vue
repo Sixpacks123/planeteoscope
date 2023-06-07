a4e18c22955dcb437d74fa07a39ca244b7ff5df0
Heurtault Aubin <aub.heurtault@gmail.com>
2023-06-07 10:24:59 +0200

    add new pages

---
+++ src/views/Favorites.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container mx-auto py-10">
+    <h1 class="text-3xl font-bold mb-4">
+      Favorite Astres
+    </h1>
+    <ul>
+      <li
+        v-for="(astre, index) in favoriteAstres"
+        :key="index"
+        class="mb-2"
+      >
+        {{ astre }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { useAstresStore } from '../store/astre'
+
+const astresStore = useAstresStore()
+const favoriteAstres = astresStore.getFavorites()
+
+console.log(favoriteAstres)
+</script>
