880ee33701f1538f89d23453d6ff08442ec3d856
Heurtault Aubin <aub.heurtault@gmail.com>
2023-06-20 15:39:08 +0200

    update route

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,4 @@
 import Home from './views/Home.vue'
-import About from './views/About.vue'
 import NotFound from './views/NotFound.vue'
 import Astres from './views/Astres.vue'
 import AstreDetail from './views/AstreDetail.vue'
@@ -11,8 +10,6 @@ export const routes = [
   { path: '/', component: Home, meta: { title: 'Home' }},
   { path: '/astres', component: Astres, meta: { title: 'Astres' }},
   { path: '/astres/:id', name: 'AstreDetail', component: AstreDetail, props: true },
-  { path: '/about', component: About, meta: { title: 'About' }},
-  { path: '/fav', component: Favorites, meta: { title: 'Liste des fav' }},
   { path: '/system', component: system, meta: { title: 'Liste des fav' }},
   { path: '/images', component: ImageJour, meta: { title: 'Image du jours'}},
   { path: '/:pathMatch(.*)*', component: NotFound, meta: { title: 'Page not found' }},
